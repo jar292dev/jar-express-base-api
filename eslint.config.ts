88dd7d943f474a34d7f56c7563f6bf1e97a3067f
Jar <jar292dev@gmail.com>
2026-03-29 23:21:59 +0200

    chore: Configurar esLint para analisis de codigo y resaltar errores

---
+++ eslint.config.ts
@@ -0,0 +1,35 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      // Variables y código muerto
+      'no-unused-vars': 'off',                        // desactivar la base (usa la de TS)
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',   // evitar usar 'any'
+
+      // Buenas prácticas
+      'eqeqeq': 'error',                              // === obligatorio
+      'no-var': 'error',                              // prohibir var
+      'prefer-const': 'warn',                         // preferir const
+      'no-console': 'warn',                           // recordar quitar logs
+
+      // Estilo
+      'semi': ['error', 'always'],                    // punto y coma obligatorio
+      'quotes': ['error', 'single'],                  // comillas simples
+    },
+    ignores: ['node_modules/', 'dist/'],
+  },
+]);
